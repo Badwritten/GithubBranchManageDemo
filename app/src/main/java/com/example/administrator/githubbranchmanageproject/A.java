284d96fdae89f4e3c27916fa24da78d590a1dfe3
gongwenbo <1071259226@qq.com>
2018-03-13 20:09:27 +0800

    1

---
+++ app/src/main/java/com/example/administrator/githubbranchmanageproject/A.java
@@ -0,0 +1,8 @@
+package com.example.administrator.githubbranchmanageproject;
+
+/**
+ * Created by ${GongWenbo} on 2018/3/13 0013.
+ */
+
+public class A {
+}
--- app/src/main/java/com/example/administrator/githubbranchmanageproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubbranchmanageproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.githubbranchmanageproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("whs", "1");
         while (true){
             System.out.println("HelloWord");
         }
-        Log.d("whs", "1");
     }
 }
