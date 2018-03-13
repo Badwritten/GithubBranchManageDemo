ce6c6f53487b9fe6aa7cd141948c0b14a623b131
wanghaishu <1489252049@qq.com>
2018-03-13 23:35:13 +0800

    第一次提交

--- app/src/main/java/com/example/administrator/githubbranchmanageproject/MainActivity.java
+++ app/src/main/java/com/example/administrator/githubbranchmanageproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.githubbranchmanageproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("whs", "1");
+        //Alexander第一次提交
         while (true){
             System.out.println("HelloWord");
         }
-        Log.d("whs", "1");
     }
 }
